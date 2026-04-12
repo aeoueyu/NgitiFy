401f6b5a9ed9a0a881cc2fcf14bd2f6ec43326a2
Aeiou Nicole Garcia <garciaaeiounicole@gmail.com>
2026-04-12 13:15:45 +0800

    cors

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+    origin: ['https://ngitify.com', 'https://www.ngitify.com'],
+    credentials: true, 
+};
+app.use(cors(corsOptions));
+
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
